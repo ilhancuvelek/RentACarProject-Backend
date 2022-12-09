636f74c687cdb043894e0e194fff746046146d45
İlhan Cüvelek <cuvelekilhan@gmail.com>
2022-12-09 15:06:58 +0300

    Kamp 12.Gün

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Entityframework;
+using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    //startup ta yaptığımızı burda yapıyoruz
+    //IOC yapılandırması yaptık
+    //program.cs->IHostBuilder da tanımladık burayı
+    public class AutofacBusinessModule:Module
+    {
+        //uygulama çalıştığında bu çalışır.
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
